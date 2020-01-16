9f39829c3cd551347acfb3292ff90758283102d3
Christian Norrman <christian@faxity.se>
2020-01-16 16:51:11 +0100

    Fixed link in tag view

--- view/site/tags/tag.php
+++ view/site/tags/tag.php
@@ -15,7 +15,7 @@ namespace Anax\View;
 <ul class="questions">
     <?php foreach ($questions as $question) : ?>
         <li>
-            <a href="<?= url("questions/$question->id") ?>">
+            <a href="<?= url("question/$question->id") ?>">
                 <?= $question->title ?>
             </a>
         </li>
